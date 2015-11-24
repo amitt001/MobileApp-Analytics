e0e1c47cec7346d0d02c38e52b8ff0d15089dfd5
amitt001 <mail2amit19@gmail.com>
2015-11-24 11:17:54 +0530

    elasticsearch search

--- appdecodeweb/userapp/search.py
+++ appdecodeweb/userapp/search.py
@@ -2,11 +2,23 @@
 Search the apps by name and ID. App data is stored in mongo. 
 For search elastic search is used
 """
+import elasticsearch
 
+
+DB = 'playdb'
 
 class Search(object):
     """
     Search apps by name or ID of apps
     """
+    def __init__(self):
+        # use default settings
+        self.es = elasticsearch.Elasticsearch()
+
     def search(self, q):
-        return [1,2,3,4]
+        try:
+            result = self.es.search(index=DB, q=q)
+        except elasticsearch.ConnectionError as e:
+            print(e)
+            return ['ElasticSearch connectionError: Failed to establish a new connection: Connection refused']
+        return [r['_id'] for r in result['hits']['hits']]
