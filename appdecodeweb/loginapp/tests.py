4e8568321d456afdd2e0689c160567fa1c1e60be
amitt001 <mail2amit19@gmail.com>
2015-12-23 00:52:30 +0530

    simple unit tests to check form

--- appdecodeweb/loginapp/tests.py
+++ appdecodeweb/loginapp/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from loginapp.forms import *
+
+class IndexTests(TestCase):
+    """To test index page"""
+    def test_index_renders(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+
+class SignUpTest(TestCase):
+    """Form test"""
+
+    def register_test_forms(self):
+        """User registeration testing"""
+        form_data = {'username': 'testuser',
+            'email': 'testemail@test.com',
+            'password1': '1234',
+            'password2': '1234'}
+        form = RegistrationForm(form_data)
+        self.assertTrue(form.is_valid())
+
+    def setUp(self):
+    	"""Setup for logging in"""
+        self.credentials = {'username': 'testuser',
+            'password': '1234'}
+        User.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        # send login data
+        response = self.client.post('/user/accounts/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
